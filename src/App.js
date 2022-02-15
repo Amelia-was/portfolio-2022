51c96f64da0004cb2db6e1bf1100f797d72020ff
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-02-15 10:07:08 -0500

    about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React from 'react';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amelia Wasowski</h1>
-    </div>
+    <section className='page'>
+
+      <header>
+        <h1>amelia wasowski</h1>
+      </header>
+
+      <main>
+        <About></About>
+      </main>
+
+      <footer>
+        stuff
+      </footer>
+
+    </section>
   );
 }
 
---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className='about'>
+            <p className='about-text'>Hi! I'm a junior web developer.</p>
+        </section>
+    );
+};
+
+export default About;
