1b23bd02011ba9f430293d8a19ffc53d0e9058ee
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-02-15 10:13:04 -0500

    about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
     return (
         <section className='about'>
-            <p className='about-text'>Hi! I'm a junior web developer.</p>
+            <p className='about-text'>Hi! I'm a junior web developer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quam tellus, egestas eget purus at, scelerisque faucibus turpis. Maecenas bibendum felis rhoncus auctor maximus. Quisque a consectetur magna, at rutrum nunc.</p>
         </section>
     );
 };
