9d10a4f8f2e4fa9053625264b8cd38f41b60b70a
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-02-16 10:15:17 -0500

    add project component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,51 @@
 import React, { useState } from 'react';
+import Project from '../Project';
 
 function Portfolio() {
+    const [apps] = useState([
+        {
+            name: 'BookEnd',
+            altURL: 'http://bookend-recommendations.herokuapp.com/',
+            altRepo: 'https://github.com/project3UOT/bookend',
+            featured: true
+        },
+        {
+            name: 'Budget Tracker',
+            altURL: 'https://blooming-tor-59716.herokuapp.com/'
+        },
+        {
+            name: 'Swatch',
+            altRepo: 'https://github.com/Project-Two-Group-Nine/SWATCH',
+            altURL: 'https://swatch-makeup-reviews.herokuapp.com/'
+        },
+        {
+            name: 'Tech Blog',
+            altURL: 'https://sleepy-scrubland-82236.herokuapp.com/'
+        },
+        {
+            name: 'Team Profile Generator'
+        },
+        {
+            name: 'Note Taker',
+            altURL: 'https://polar-sea-04536.herokuapp.com/'
+
+        },
+        {
+            name: 'Weather Dashboard'
+        },
+        {
+            name: 'Work Day Schedule'
+        },
+        {
+            name: 'Taskmaster Pro'
+        }
+    ])
+
     return (
-        <section>
-            <ul>portfolio</ul>
+        <section className='portfolio-container justify-center'>
+            {apps.map((app, i) =>
+                <Project name={app.name} index={(apps.length - i - 1)} altRepo={app.altRepo} altURL={app.altURL} notDeployed={app.notDeployed} featured={app.featured} />
+            )}
         </section>
     );
 };
